c66c88988d589ca22196838e0043b8ae755c274a
TeamSixOrg <daniel_12x@outlook.com>
2016-10-15 19:53:22 -0300

    Ajust.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+#region Using directives
+using System;
+using System.Reflection;
+using System.Runtime;
+using System.Runtime.InteropServices;
+
+#endregion
+[assembly: AssemblyTitle ("Adcrip")]
+[assembly: AssemblyDescription ("Biblioteca de criptografia assimétrica.")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("TeamSix")]
+[assembly: AssemblyProduct ("Adcrip")]
+[assembly: AssemblyCopyright ("Copyright© TeamSix 2016")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: ComVisible (true)]
+[assembly: AssemblyVersion ("1.3.0")]
+[assembly: AssemblyFileVersion ("1.3.0")]
